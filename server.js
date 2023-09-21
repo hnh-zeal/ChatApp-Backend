662351362e74220fb93d004d2452d4d4bd13735c
Innovix <htetnainghein@innovix-solutions.com>
2023-09-22 00:07:55 +0630

    Database Connected

--- server.js
+++ server.js
@@ -1,23 +1,24 @@
 const app = require("./app");
-require("dotenv").config();
+const connectDatabase = require('./database/database');
+const dotenv = require("dotenv");
+dotenv.config();
 
 process.on("uncaughtException", (err) => {
   console.log(`Error: ${err.message}`);
   process.exit(1);
 });
 
-
 // Connect Database
-
+connectDatabase();
 
 const http = require("http");
 
 const server = http.createServer(app);
 
-const port = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8000;
 
-server.listen(port, () => {
-  console.log(`App is running on port ${port}`);
+server.listen(PORT, () => {
+  console.log(`App is running on port ${PORT}`);
 });
 
 process.on("unhandledRejection", (err) => {
